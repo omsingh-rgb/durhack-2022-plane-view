976deb02e2ff80b74a047f7905c42a61da068bc7
David Fairbairn <david.l.fairbairn@outlook.com>
2022-11-19 17:40:48 +0000

    Update

--- app/google_earth.py
+++ app/google_earth.py
@@ -1,11 +1,32 @@
 import ee
 import private
-def main():
-    # Trigger the authentication flow.
-    ee.Authenticate(auth_mode="notebook")
+import matplotlib.pyplot as plt
+import pydeck as pdk
+from pydeck_earthengine_layers import EarthEngineTerrainLayer
 
+def main():
     # Initialize the library.
-    ee.Initialize()
+    try:
+        ee.Initialize()
+    except:
+        ee.Authenticate(auth_mode="notebook")
+        ee.Initialize()
+
+    image = ee.Image('USGS/NED').select('elevation')
+    terrain = ee.Image('USGS/NED').select('elevation')
+    vis_params = {
+        "min": 0,
+        "max": 4000,
+        "palette": ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5'],
+    }
+    ee_layer = EarthEngineTerrainLayer(image, terrain, vis_params, id="EETerrainLayer")
+    view_state = pdk.ViewState(
+        latitude=36.15, longitude=-111.96, zoom=10.5, bearing=-66.16, pitch=60
+    )
+
+    r = pdk.Deck(layers=[ee_layer], initial_view_state=view_state)
+
+    r.to_html("test.html", open_browser=True)
 
 if __name__=="__main__":
     main()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 
 from flight_data import get_flight_details
+import cesiumpy
 
 def main():
-    pass
+    v = cesiumpy.Viewer()
+    v.entities.add(cesiumpy.Box(dimensions=(40e4, 30e4, 50e4), material=cesiumpy.color.RED, position=(-120, 40, 0)))
 
 if __name__=="__main__":
     pass
